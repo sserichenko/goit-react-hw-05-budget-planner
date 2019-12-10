097bd311faad2454edee4180bbbfdac5c3cd5ce7
sserichenko <sserichenko@gmail.com>
2019-12-10 14:49:22 +0200

    first commit

---
+++ build/precache-manifest.5d93149c421a613d6f7bbdb4b1595f8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7ac35cd1449942583b386c8579740ad",
+    "url": "./index.html"
+  },
+  {
+    "revision": "fc1d10eb76ee1907279e",
+    "url": "./static/css/main.67e7994e.chunk.css"
+  },
+  {
+    "revision": "991da6611c242bab1367",
+    "url": "./static/js/2.869e32a3.chunk.js"
+  },
+  {
+    "revision": "fc1d10eb76ee1907279e",
+    "url": "./static/js/main.76e853eb.chunk.js"
+  },
+  {
+    "revision": "f445c3fe61ce6071d839",
+    "url": "./static/js/runtime~main.48b12a93.js"
+  }
+]);
---
+++ src/redux/types.js
@@ -0,0 +1,10 @@
+const types = {
+  BUDGET_ADD: 'BUDGET_ADD',
+
+  // BudgetNotes
+
+  BUDGET_NOTE_ADD: 'BUDGET_NOTE_ADD',
+  BUDGET_NOTE_DELETE: 'BUDGET_NOTE_DELETE',
+};
+
+export default types;
